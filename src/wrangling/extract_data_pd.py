a6b427286dc39b8d34fd421841450fd103d5a447
RalleyD <0ralld67@solent.ac.uk>
2024-12-22 15:24:23 +0000

    COM731 - task b2 refactor
    move wrangling into function that nicely prints the output
    
    extract_data_pd.py
    - groups the given dataset by ethnicity
    - gets the specific group based on the input parameter; groups by treatment gets the average white blood cell count for each treatment group.

--- src/wrangling/extract_data_pd.py
+++ src/wrangling/extract_data_pd.py
@@ -29,3 +29,23 @@ def patient_long_survival(ethnicity: str, lung_cancer_df: pd.DataFrame):
         f"Top three treatments for {ethnicity} group - Surival > {survival_months} months")
     print(long_term_ethnic_groups.get_group(
         ethnicity).Treatment.value_counts().head(3))
+
+
+def treatment_white_blood_count(ethnicity: str, lung_cancer_df: pd.DataFrame):
+    """
+    Print the average white blood cell count for each treatment type
+    given a certain ethnicity.
+
+    Args:
+        ethnicity (str): case-insensitive ethnic group.
+        lung_cancer_df (DataFrame): lung cancer Pandas DataFrame.
+    """
+    ethnicity = _capitalise_input(ethnicity)
+
+    white_blood_df = lung_cancer_df.groupby('Ethnicity')
+
+    print(
+        f"Average white blood cell count for each treatment in {ethnicity} ethnic group")
+
+    print(white_blood_df.get_group(ethnicity).groupby(
+        'Treatment').White_Blood_Cell_Count.mean())
